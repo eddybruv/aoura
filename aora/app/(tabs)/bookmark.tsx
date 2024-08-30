928266f4d2c420b7b1eceab13aeca30e56fd8e63
Edwin Ajong <edwinajong@gmail.com>
2024-08-29 22:13:42 -0500

    tabs

---
+++ aora/app/(tabs)/bookmark.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Bcookmark = () => {
+  return (
+    <View>
+      <Text>Bcookmark</Text>
+    </View>
+  );
+};
+
+export default Bcookmark;
