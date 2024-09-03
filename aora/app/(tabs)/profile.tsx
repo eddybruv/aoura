0f7849a72ec45eb09b5bdee24d343103400ee823
Edwin Ajong <edwinajong@gmail.com>
2024-09-03 07:38:49 -0500

    new profile tab

--- aora/app/(tabs)/profile.tsx
+++ aora/app/(tabs)/profile.tsx
@@ -1,11 +1,84 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, FlatList, TouchableOpacity, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../components/EmptyState";
+import { getUserPosts, signOut } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
+import VideoCard from "../components/VideoCard";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import { icons } from "@/constants";
+import InfoBox from "../components/InfoBox";
+import { router } from "expo-router";
 
 const Profile = () => {
+  const { user, setIsLoggedIn, setUser } = useGlobalContext();
+
+  const { data: posts, refetch } = useAppwrite(() => getUserPosts(user.$id));
+  const [refreshing, setRefreshing] = useState(false);
+
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+
+    router.replace("/sign-in");
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item: any) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => {
+          return (
+            <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+              <TouchableOpacity
+                className="w-full items-end mb-10"
+                onPress={logout}
+              >
+                <Image
+                  source={icons.logout}
+                  resizeMode="contain"
+                  className="w-6 h-6"
+                />
+              </TouchableOpacity>
+              <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  resizeMode="contain"
+                  className="w-[90%] h-[90%] rounded-lg"
+                />
+              </View>
+              <InfoBox
+                title={user?.username}
+                containerStyles="mt-5"
+                titleStyles={"text-lg"}
+              />
+              <View className="mt-5 flex-row">
+                <InfoBox
+                  title={posts.length || 0}
+                  containerStyles="mr-10"
+                  titleStyles={"text-xl"}
+                  subtitle={"Posts"}
+                />
+                <InfoBox
+                  title={"1.2k"}
+                  subtitle="Followers"
+                  titleStyles={"text-xl"}
+                />
+              </View>
+            </View>
+          );
+        }}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this query"
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
